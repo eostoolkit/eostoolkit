66d1273ecdef4e518b43cfc02bce643ccfe5714c
Denis <carriere.denis@gmail.com>
2018-06-23 11:28:52 -0400

    Remove Refund & Remove Tests

--- app/containers/Refund/tests/reducer.test.js
+++
@@ -1,8 +0,0 @@
-import { fromJS } from 'immutable';
-import refundReducer from '../reducer';
-
-describe('refundReducer', () => {
-  it('returns the initial state', () => {
-    expect(refundReducer(undefined, {})).toEqual(fromJS({}));
-  });
-});
