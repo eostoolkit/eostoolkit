ba341b7e56f1de721c9268d5edd4d32c95e6bc71
Nathan Rempel <nathan.rempel@solium.com>
2018-06-26 08:19:09 -0600

    Add EOX airdrop

--- app/eosTokens.js
+++ app/eosTokens.js
@@ -2,6 +2,6 @@
 // Provide a list of EOS tokens that are available
 //
 
-const eosTokens = ['eosio.token', 'eosadddddddd', 'eosdactokens', 'gyztomjugage'];
+const eosTokens = ['eosio.token', 'eosadddddddd', 'eosdactokens', 'gyztomjugage', 'eoxeoxeoxeox'];
 
 export default eosTokens;
