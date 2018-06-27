5c2ff02db5f93a02851d28d79d7c5ec61cecf6b4
Nathan Rempel <nathan.rempel@solium.com>
2018-06-27 12:04:59 -0600

    Add eosiochaince airdrop

--- app/eosTokens.js
+++ app/eosTokens.js
@@ -2,6 +2,6 @@
 // Provide a list of EOS tokens that are available
 //
 
-const eosTokens = ['eosio.token', 'eosadddddddd', 'eosdactokens', 'gyztomjugage', 'eoxeoxeoxeox'];
+const eosTokens = ['eosio.token', 'eosadddddddd', 'eosdactokens', 'gyztomjugage', 'eoxeoxeoxeox', 'eosiochaince'];
 
 export default eosTokens;
