982f2c9c45d295283289a0216736e003ac97eb82
Nathan Rempel <nathan.rempel@solium.com>
2018-06-30 16:00:29 -0600

    Changed reducer for language provider

--- app/containers/LanguageProvider/reducer.js
+++ app/containers/LanguageProvider/reducer.js
@@ -12,7 +12,7 @@ import {DEFAULT_LOCALE} from 'i18n'; // eslint-disable-line
 const browserLang = navigator ? navigator.language || navigator.browserLanguage || DEFAULT_LOCALE : DEFAULT_LOCALE;
 
 const initialState = fromJS({
-  locale: browserLang.substring(0,2),
+  locale: browserLang.substring(0, 2),
 });
 
 function languageProviderReducer(state = initialState, action) {
