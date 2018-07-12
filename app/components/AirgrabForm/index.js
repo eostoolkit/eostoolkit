5786179cdf3e3f19a746a943e939c31fe4bf111d
Nathan Rempel <nathan.rempel@solium.com>
2018-07-12 16:38:33 -0600

    Update airdrop message

--- app/components/AirgrabForm/index.js
+++ app/components/AirgrabForm/index.js
@@ -107,7 +107,7 @@ const AirgrabForm = props => {
             <h5>The POORMANTOKEN is an EOS token that includes signups and burning to make airdrops cheaper</h5>
             <p>The intention is to allow EOS accounts to Airgrab the token (i.e. opt-in or signup)</p>
             <p>You can participate in this test by using this Airgrab form.</p>
-            <p>POOR tokens will be dropped on a 1:1 POOR:EOS basis on Wednesday, July 11</p>
+            <p>The July 11 drop was successful! More will be announced later, keep registering so you can catch up!</p>
             <p>There may be future drops to test different airdrop and signup criteria</p>
             <p>
               To read more about this token contract check out the{' '}
