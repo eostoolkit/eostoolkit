f12e64a87bea238b9bcf17a690ab026cd26f4b86
generEOS <39075703+generEOS@users.noreply.github.com>
2018-07-26 11:23:24 +1000

    Fix lookup of system accounts (#53)

--- app/components/AccountTables/resources.js
+++ app/components/AccountTables/resources.js
@@ -33,28 +33,40 @@ function ResourcesTable({ ...props }) {
             <TableCell className={classes.tableCell}>RAM</TableCell>
             <TableCell className={classes.tableCell}>
               {account.ram_usage} bytes used<br />
-              {account.ram_quota} bytes owned
+              {account.ram_quota === -1 ? 'Unlimited' : account.ram_quota} bytes owned
             </TableCell>
           </TableRow>
           <TableRow className={`${classes.tableRowHover}`}>
             <TableCell className={classes.tableCell}>CPU</TableCell>
             <TableCell className={classes.tableCell}>
-              {account.total_resources.cpu_weight}
-              <br />({Number((account.cpu_limit.used / account.cpu_limit.max) * 100).toFixed(2)} % used)
+              {account && account.total_resources ? (
+                <span>{account.total_resources.cpu_weight}
+                <br />({Number((account.cpu_limit.used / account.cpu_limit.max) * 100).toFixed(2)} % used)</span>
+              ) : (
+                <span>Unlimited</span>
+              )}
             </TableCell>
           </TableRow>
           <TableRow className={`${classes.tableStripedRow} ${classes.tableRowHover}`}>
             <TableCell className={classes.tableCell}>NET</TableCell>
             <TableCell className={classes.tableCell}>
-              {account.total_resources.net_weight}
-              <br />({Number((account.net_limit.used / account.net_limit.max) * 100).toFixed(2)} % used)
+              {account && account.total_resources ? (
+                <span>{account.total_resources.net_weight}
+                <br />({Number((account.net_limit.used / account.net_limit.max) * 100).toFixed(2)} % used)</span>
+              ) : (
+                <span>Unlimited</span>
+              )}
             </TableCell>
           </TableRow>
           <TableRow className={`${classes.tableRowHover}`}>
-            <TableCell className={classes.tableCell}>REFUND</TableCell>
+            <TableCell className={classes.tableCell}>REFUNDING</TableCell>
             <TableCell className={classes.tableCell}>
-              CPU: {account.refund_request ? account.refund_request.cpu_amount : '0.0000 EOS'}
-              <br />NET: {account.refund_request ? account.refund_request.net_amount : '0.0000 EOS'}
+              {account && account.refund_request ? (
+                <span>CPU: {account.refund_request.cpu_amount}
+                <br />NET: {account.refund_request.net_amount}</span>
+              ) : (
+                <span>None</span>
+              )}
             </TableCell>
           </TableRow>
         </TableBody>
