25eaebc1ae705947bf26cc624b72f61f1195370d
Nathan Rempel <nathan.rempel@solium.com>
2018-08-23 13:04:49 +1000

    Add metadata zendesk and ATD airgrab

--- app/remoteConfig.js
+++ app/remoteConfig.js
@@ -4,6 +4,13 @@
 
 const airgrabs = [
   {
+    symbol: 'ATD',
+    account: 'eosatidiumio',
+    method: 'signup',
+    description: 'Payments & Budget Management Decentralized App Leveraging the Blockchain, Cryptocurrency and AI Technologies. Drops happen every 24 hours, Airgrab Today!',
+    url: 'https://www.atidium.io/',
+  },
+  {
     symbol: 'POOR',
     account: 'poormantoken',
     method: 'signup',
