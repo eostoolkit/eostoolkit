63504fc308346257e1224144291065fa75fe9c8c
Andre Litty <andre.litty@gmx.de>
2018-12-20 19:19:51 +1100

    Updated grandpa index to latest version

--- app/components/Features/Grandpa/index.js
+++ app/components/Features/Grandpa/index.js
@@ -18,21 +18,14 @@ import Transfer from './TransferForm';
 import Info from '@material-ui/icons/Info';
 import GrandpaInfo from 'components/Information/Grandpa';
 
-import { FormattedMessage } from 'react-intl';
-
-import messages from './messages';
-
-const makeClaim = (values, networkIdentity, referrer) => {
-  const data = {
-    owner: networkIdentity ? networkIdentity.name : '',
-    sym: `4,${values}`,
-    referrer,
-  };
+const makeClaim = (values, networkIdentity) => {
   const transaction = [
     {
       account: 'grandpacoins',
-      name: 'mine',
-      data,
+      name: 'quit',
+      data: {
+        owner: networkIdentity ? networkIdentity.name : '',
+      },
     },
   ];
   return transaction;
@@ -40,48 +33,21 @@ const makeClaim = (values, networkIdentity, referrer) => {
 
 const Grandpa = props => {
   const { pushTransaction, networkIdentity, networkAccount, miner, intl } = props;
-  const referrer = props.location.search ? props.location.search.split('=')[1] : '';
 
   const handleClaims = values => {
-    const transaction = makeClaim(values, networkIdentity, referrer);
+    const transaction = makeClaim(values, networkIdentity);
     pushTransaction(transaction, props.history);
   };
 
-  const refLink = `https://eostoolkit.io/grandpacoins?r=${networkIdentity ? networkIdentity.name : 'youracctname'}`;
-
   return (
     <Tool>
-      <ToolSection lg={12}>
-        <ToolBody
-          color="info"
-          icon={Info}
-          header={intl.formatMessage(messages.grandpaIndexHeader)}
-          subheader={intl.formatMessage(messages.grandpaIndexSubHeader)}>
+      <ToolSection lg={6}>
+        <ToolBody color="info" icon={Info} header="GrandpaCoins">
           <GrandpaInfo />
-          <h5>
-            <FormattedMessage {...messages.grandpaIndexReferralLinkText} />
-            <a href={refLink} target="new">
-              {refLink}
-            </a>
-          </h5>
         </ToolBody>
       </ToolSection>
       <ToolSection lg={6}>
-        <MineTable
-          tokens={['BTC', 'ETH', 'DOGE']}
-          stats={miner ? miner.stats : null}
-          handleSubmit={handleClaims}
-          referrer={referrer}
-          intl={intl}
-        />
-        <MinerTable intl={intl} {...props} />
-        <Usurp {...props} />
-        <Transfer {...props} />
-      </ToolSection>
-      <ToolSection lg={6}>
-        <TokenTable token={miner ? miner.stats.find(s => s.token === 'BTC') : null} symbol={'BTC'} intl={intl} />
-        <TokenTable token={miner ? miner.stats.find(s => s.token === 'ETH') : null} symbol={'ETH'} intl={intl} />
-        <TokenTable token={miner ? miner.stats.find(s => s.token === 'DOGE') : null} symbol={'DOGE'} intl={intl} />
+        <MineTable handleSubmit={handleClaims} intl={intl}/>
       </ToolSection>
     </Tool>
   );
