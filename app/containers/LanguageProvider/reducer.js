43a5d0a3693bdbcd1d8fabf7643db37b44213e7d
Nathan Rempel <nsrempel@gmail.com>
2018-12-27 16:34:32 +1100

    Some tweaks to translation setup and add ko/zh files

--- app/containers/LanguageProvider/reducer.js
+++ app/containers/LanguageProvider/reducer.js
@@ -4,25 +4,24 @@
  *
  */
 
-import { fromJS } from 'immutable';
+ import { fromJS } from 'immutable';
 
-import { CHANGE_LOCALE } from './constants';
-import { DEFAULT_LOCALE } from '../App/constants'; // eslint-disable-line
+ import { CHANGE_LOCALE } from './constants';
+ import {DEFAULT_LOCALE} from 'i18n'; // eslint-disable-line
 
-const browserLang = navigator ? navigator.language || navigator.browserLanguage || DEFAULT_LOCALE : DEFAULT_LOCALE;
+ const browserLang = navigator ? navigator.language || navigator.browserLanguage || DEFAULT_LOCALE : DEFAULT_LOCALE;
 
-const initialState = fromJS({
-  locale: browserLang.substring(0,2),
-});
+ const initialState = fromJS({
+   locale: browserLang.substring(0, 2),
+ });
 
-function languageProviderReducer(state = initialState, action) {
-  switch (action.type) {
-    case CHANGE_LOCALE:
-      return state.set('locale', action.locale);
-    default:
-      return state;
-  }
-}
+ function languageProviderReducer(state = initialState, action) {
+   switch (action.type) {
+     case CHANGE_LOCALE:
+       return state.set('locale', action.locale);
+     default:
+       return state;
+   }
+ }
 
-export default languageProviderReducer;
-
+ export default languageProviderReducer;
--- app/i18n.js
+++ app/i18n.js
@@ -4,43 +4,46 @@
  * This will setup the i18n language files and locale data for your app.
  *
  */
-import { addLocaleData } from 'react-intl';
+ const addLocaleData = require('react-intl').addLocaleData; //eslint-disable-line
+ const enLocaleData = require('react-intl/locale-data/en');
+ const zhLocaleData = require('react-intl/locale-data/zh');
+ const koLocaleData = require('react-intl/locale-data/ko');
 
-/*
-* Import Locale data if specific locales are needed
-* */
-import enLocaleData from 'react-intl/locale-data/en';
-import deLocaleData from 'react-intl/locale-data/de';
+ const enTranslationMessages = require('./translations/en.json');
+ const zhTranslationMessages = require('./translations/zh.json');
+ const koTranslationMessages = require('./translations/ko.json');
 
-import { DEFAULT_LOCALE } from './containers/App/constants'; // eslint-disable-line
+ addLocaleData(enLocaleData);
+ addLocaleData(zhLocaleData);
+ addLocaleData(koLocaleData);
 
-/*
-* Import translation files here after translating all application strings in country specific translation json file
-* */
-import enTranslationMessages from './translations/en.json';
-import deTranslationMessages from './translations/de.json';
+ const DEFAULT_LOCALE = 'en';
 
-export const appLocales = ['en', 'de'];
+ // prettier-ignore
+ const appLocales = [
+   'en',
+   'ko',
+   'zh',
+ ];
 
-addLocaleData(enLocaleData);
-addLocaleData(deLocaleData);
+ const formatTranslationMessages = (locale, messages) => {
+   const defaultFormattedMessages =
+     locale !== DEFAULT_LOCALE ? formatTranslationMessages(DEFAULT_LOCALE, enTranslationMessages) : {};
+   const flattenFormattedMessages = (formattedMessages, key) => {
+     const formattedMessage =
+       !messages[key] && locale !== DEFAULT_LOCALE ? defaultFormattedMessages[key] : messages[key];
+     return Object.assign(formattedMessages, { [key]: formattedMessage });
+   };
+   return Object.keys(messages).reduce(flattenFormattedMessages, {});
+ };
 
-export const formatTranslationMessages = (locale, messages) => {
-  const defaultFormattedMessages =
-    locale !== DEFAULT_LOCALE ? formatTranslationMessages(DEFAULT_LOCALE, enTranslationMessages) : {};
-  return Object.keys(messages).reduce((formattedMessages, key) => {
-    let message = messages[key];
-    if (!message && locale !== DEFAULT_LOCALE) {
-      message = defaultFormattedMessages[key];
-    }
-    return Object.assign(formattedMessages, { [key]: message });
-  }, {});
-};
+ const translationMessages = {
+   en: formatTranslationMessages('en', enTranslationMessages),
+   zh: formatTranslationMessages('zh', zhTranslationMessages),
+   ko: formatTranslationMessages('ko', koTranslationMessages),
+ };
 
-/*
-* Do not forget to add new translation messages!!
-* */
-export const translationMessages = {
-  en: formatTranslationMessages('en', enTranslationMessages),
-  de: formatTranslationMessages('de', deTranslationMessages),
-};
+ exports.appLocales = appLocales;
+ exports.formatTranslationMessages = formatTranslationMessages;
+ exports.translationMessages = translationMessages;
+ exports.DEFAULT_LOCALE = DEFAULT_LOCALE;
