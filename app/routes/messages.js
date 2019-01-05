42c1ccf2ca7ad50cde0fd684b418706dbdd06a71
Andre Litty <andre.litty@gmx.de>
2019-01-06 02:20:07 +1100

    Adjusted routes to use i18n messages, added routes messages

---
+++ app/routes/messages.js
@@ -0,0 +1,157 @@
+/*
+ * Home Messages
+ *
+ * This contains all the text for the Home component.
+ */
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  menuFeatures: {
+    id: 'app.routes.menuFeatures',
+    defaultMessage: 'Features',
+  },
+  menuReferendum: {
+    id: 'app.routes.menuReferendum',
+    defaultMessage: 'Referendum',
+  },
+  menuPoorSwap: {
+    id: 'app.routes.menuPoorSwap',
+    defaultMessage: 'PoorSwap',
+  },
+  menuDonate: {
+    id: 'app.routes.menuDonate',
+    defaultMessage: 'Donate',
+  },
+  menuGovernance: {
+    id: 'app.routes.menuGovernance',
+    defaultMessage: 'Governance',
+  },
+  menuDapps: {
+    id: 'app.routes.menuDapps',
+    defaultMessage: 'Dapps',
+  },
+  menuKARMA: {
+    id: 'app.routes.menuKARMA',
+    defaultMessage: 'KARMA',
+  },
+  menuHorusPay: {
+    id: 'app.routes.menuHorusPay',
+    defaultMessage: 'HorusPay',
+  },
+  menuGrandpaCoins: {
+    id: 'app.routes.menuGrandpaCoins',
+    defaultMessage: 'GrandpaCoins',
+  },
+  menuAirgrabTokens: {
+    id: 'app.routes.menuAirgrabTokens',
+    defaultMessage: 'Airgrab Tokens',
+  },
+  menuFindAccounts: {
+    id: 'app.routes.menuFindAccounts',
+    defaultMessage: 'Find Accounts',
+  },
+  menuCreateAccount: {
+    id: 'app.routes.menuCreateAccount',
+    defaultMessage: 'Create Account',
+  },
+  menuTransferTokens: {
+    id: 'app.routes.menuTransferTokens',
+    defaultMessage: 'Transfer Tokens',
+  },
+  menuManageAccount: {
+    id: 'app.routes.menuManageAccount',
+    defaultMessage: 'Manage Account',
+  },
+  menuManageStake: {
+    id: 'app.routes.menuManageStake',
+    defaultMessage: 'Manage Stake',
+  },
+  menuManageRAM: {
+    id: 'app.routes.menuManageRAM',
+    defaultMessage: 'Manage RAM',
+  },
+  menuManagePermissions: {
+    id: 'app.routes.menuManagePermissions',
+    defaultMessage: 'Manage Permissions',
+  },
+  menuAdvancedPermissions: {
+    id: 'app.routes.menuAdvancedPermissions',
+    defaultMessage: 'Advanced Permissions',
+  },
+  menuLinkAuth: {
+    id: 'app.routes.menuLinkAuth',
+    defaultMessage: 'Link Auth',
+  },
+  menuRefundStake: {
+    id: 'app.routes.menuRefundStake',
+    defaultMessage: 'Refund Stake',
+  },
+  menuManageVoting: {
+    id: 'app.routes.menuManageVoting',
+    defaultMessage: 'Manage Voting',
+  },
+  menuVoteProducers: {
+    id: 'app.routes.menuVoteProducers',
+    defaultMessage: 'Vote Producers',
+  },
+  menuProxyInformation: {
+    id: 'app.routes.menuProxyInformation',
+    defaultMessage: 'Proxy Information',
+  },
+  menuSetProxy: {
+    id: 'app.routes.menuSetProxy',
+    defaultMessage: 'Set Proxy',
+  },
+  menuCreateProxy: {
+    id: 'app.routes.menuCreateProxy',
+    defaultMessage: 'Create Proxy',
+  },
+  menuResignProxy: {
+    id: 'app.routes.menuResignProxy',
+    defaultMessage: 'Resign Proxy',
+  },
+  menuCommunityFeatures: {
+    id: 'app.routes.menuCommunityFeatures',
+    defaultMessage: 'Community Features',
+  },
+  menuForumStatus: {
+    id: 'app.routes.menuForumStatus',
+    defaultMessage: 'Forum Status',
+  },
+  menuForumPost: {
+    id: 'app.routes.menuForumPost',
+    defaultMessage: 'Forum Post',
+  },
+  menuForumProposal: {
+    id: 'app.routes.menuForumProposal',
+    defaultMessage: 'Forum Proposal',
+  },
+  menuPremiumNames: {
+    id: 'app.routes.menuPremiumNames',
+    defaultMessage: 'Premium Names',
+  },
+  menuMultisigTransactions: {
+    id: 'app.routes.menuMultisigTransactions',
+    defaultMessage: 'Multisig Transactions',
+  },
+  menuCreateTransaction: {
+    id: 'app.routes.menuCreateTransaction',
+    defaultMessage: 'Create Transaction',
+  },
+  menuSignTransaction: {
+    id: 'app.routes.menuSignTransaction',
+    defaultMessage: 'Sign Transaction',
+  },
+  menuPushTransaction: {
+    id: 'app.routes.menuPushTransaction',
+    defaultMessage: 'Push Transaction',
+  },
+  menuBlockProducer: {
+    id: 'app.routes.menuBlockProducer',
+    defaultMessage: 'Block Producer',
+  },
+  menuClaimRewards: {
+    id: 'app.routes.menuClaimRewards',
+    defaultMessage: 'Claim Rewards',
+  },
+});
